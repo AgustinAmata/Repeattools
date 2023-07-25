8e7b67bf02935ccb5bea5d75334a998830ff9ced
AgustinAmata <agus.amata2002@gmail.com>
2023-07-25 13:38:11 +0200

    added descriptions

--- Repeattools/src/read_input.py
+++ Repeattools/src/read_input.py
@@ -1,5 +1,8 @@
 from csv import DictReader
 
+#Merges inputs from both RepeatMasker and TESorter. Repeats
+#of RepeatMasker that are not in TESorter are filled with columns
+#of the latter
 def merge_inputs(rm_input, te_input):
     merged_inputs = []
     te_index = {}
@@ -30,6 +33,12 @@ def merge_inputs(rm_input, te_input):
 
     return merged_inputs
 
+#Reads the .out file from RepeatMasker. Each repeat is added
+#as a dictionary, containing the data from each column as key-value pairs,
+#to a common list for the file. Additionally, it separates the class/family
+#column into two separate columns, and reallocates the columns of
+#the start and (left) of "position in repeat" of repeats whose match is in
+#the complementary strand 
 def read_repeatmasker_out(input_fhand):
     fieldnames = [
         "sw", "per div", "per del", "per ins", "seqid", "start",
@@ -67,10 +76,20 @@ def read_repeatmasker_out(input_fhand):
                 repeat_data["r start"] = true_start
                 repeat_data["r left"] = true_left
 
+            repeat_data["q left"] = repeat_data["q left"].strip("()")
+            repeat_data["r left"] = repeat_data["r left"].strip("()")
+            repeat_data["length"] = int(repeat_data["end"]) - int(repeat_data["start"])
+
             read_repeats.append(repeat_data)
 
     return read_repeats
 
+#Reads the .cls.tsv file from TESorter. Sequences that were analyzed
+#by TESorter must be previously analyzed by RepeatMasker
+#(and have to be prepared with RepeatModeler). As with the
+#previous function, this one also creates a list of dictionaries,
+#each dictionary corresponding to a repeat. It also reads data
+#contained in the #TE column, which comes from RepeatMasker
 def read_tesorter_cls_tsv(input_fhand):
     read_repeats = []
     for repeat in DictReader(input_fhand, delimiter= "\t"):
@@ -111,6 +130,8 @@ def read_tesorter_cls_tsv(input_fhand):
 
         for i in range(6):
             repeat[key_names[i]] = values[i]
+
+        repeat["length"] = int(repeat["end"]) - int(repeat["start"])
 
         repeat.pop("#te")
         read_repeats.append(repeat)
